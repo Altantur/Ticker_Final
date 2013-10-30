93f1c8725bd8e4883778797d158279bf27da87df
Turuu <turuu.br@gmail.com>
2013-10-30 16:49:56 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticker::Application.config.secret_token = '65f5c813301987744bd2ce93838a2a097c64c08823dd7ba379273060fc895859d985071082242864d2f7ce8c3e84dc5ade9b83be20ace321b96a17cfc7451c39'
