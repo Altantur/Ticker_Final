d5e2f037bffa0b7bbea86de6879b7813c86d25c1
chintogtokh <bchintogtokh@gmail.com>
2013-11-29 02:25:59 +0800

    Correct migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131126180335) do
+ActiveRecord::Schema.define(:version => 20131126183138) do
 
   create_table "admins", :force => true do |t|
     t.string   "username",   :limit => 25
