2bd8aa1f65dcd543db1400b42b384b4fdc3c1d65
Turuu <turuu.br@gmail.com>
2013-12-11 18:41:22 +0800

    Product and display

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,9 +10,25 @@ class CategoriesController < ApplicationController
     posttemp = News.where(:id => CategoryNews.where(category_id:idd).pluck(:news_id))
     @posts = posttemp.paginate(page: params[:page], order: 'created_at DESC', per_page: 2)
 
-    @temp = idd
+    @temp = ProductCategory.where("category_id = #{idd.id}")
+    @pro = {}
+    array = []
+    @temp.each do |porcat|
+       array << porcat.product_id
+     end
+     tt=""
+      array.each do |i| tt+=i.to_s + ','
+      end
+      tt =tt[0,tt.length-1]
+    sql = "select * from products where id IN (#{tt})"
+    @pro = Product.find_by_sql(sql)
+    
+    #puts "FUCKFUCK"
+    #tm = ProductCategory.where(category_id:idd.id)
+    #@pro = Product.where(:id => tm)
+    #puts "FUCKFUCK"
 
-    @loc = Location.where("product_id = #{@temp.id}") 
+    @loc = Location.where("product_id = #{idd.id}") 
     
   end
 end
